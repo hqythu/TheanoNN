6d175a867352a12a96856876d6bb4b7392b64f19
何钦尧 <hqythu@gmail.com>
2016-01-19 21:35:56 +0800

    useless code

--- theano_nn/model.py
+++ theano_nn/model.py
@@ -39,7 +39,7 @@ class Model():
         for reg in self.regularization_param:
             reg_cost = reg_cost + self.regularization * reg
 
-        self.label_predict = self.test_output #T.argmax(self.output, axis=1)
+        self.label_predict = T.argmax(self.test_output, axis=1)
 
         self.params_update = [theano.shared(param.get_value() * 0) for param in self.params]
         updates = [(param, param - self.learning_rate * param_update)
@@ -63,60 +63,8 @@ class Model():
             for start, end in zip(range(0, len(train_x), self.batch_size),
                 range(self.batch_size, len(train_x), self.batch_size)):
                 cost += [self.train(train_x[start:end], train_y[start:end])]
-            tmp = self.predict(valid_x) - valid_y
-            accuracy = np.sqrt(np.mean(tmp * tmp)) * 48
+            accuracy = np.mean(np.argmax(valid_y, axis=1) == self.predict(valid_x))
             valid_cost = self.cost_fun(valid_x, valid_y)
             if ((i+1) % self.disp_freq == 0):
                 print 'training cost:', np.mean(cost), ',', 'validation cost:', valid_cost, \
                     ',', 'accuracy:', accuracy
-
-    def save_test_result(self, test_x):
-        dic = {
-                'left_eye_center_x'         :  0,
-                'left_eye_center_y'         :  1,
-                'right_eye_center_x'        :  2,
-                'right_eye_center_y'        :  3,
-                'left_eye_inner_corner_x'   :  4,
-                'left_eye_inner_corner_y'   :  5,
-                'left_eye_outer_corner_x'   :  6,
-                'left_eye_outer_corner_y'   :  7,
-                'right_eye_inner_corner_x'  :  8,
-                'right_eye_inner_corner_y'  :  9,
-                'right_eye_outer_corner_x'  : 10,
-                'right_eye_outer_corner_y'  : 11,
-                'left_eyebrow_inner_end_x'  : 12,
-                'left_eyebrow_inner_end_y'  : 13,
-                'left_eyebrow_outer_end_x'  : 14,
-                'left_eyebrow_outer_end_y'  : 15,
-                'right_eyebrow_inner_end_x' : 16,
-                'right_eyebrow_inner_end_y' : 17,
-                'right_eyebrow_outer_end_x' : 18,
-                'right_eyebrow_outer_end_y' : 19,
-                'nose_tip_x'                : 20,
-                'nose_tip_y'                : 21,
-                'mouth_left_corner_x'       : 22,
-                'mouth_left_corner_y'       : 23,
-                'mouth_right_corner_x'      : 24,
-                'mouth_right_corner_y'      : 25,
-                'mouth_center_top_lip_x'    : 26,
-                'mouth_center_top_lip_y'    : 27,
-                'mouth_center_bottom_lip_x' : 28,
-                'mouth_center_bottom_lip_y' : 29
-              }
-
-        answer = self.predict(test_x)
-        answer = answer * 48.0 + 48
-        sio.savemat('result.mat', {'test_x': test_x,
-                                   'test_y': answer} )
-        csvfile = file('result.csv', 'wb')
-        writer = csv.writer(csvfile)
-        writer.writerow(['RowId', 'Location'])
-
-        csvmodel = file('IdLookupTable.csv', 'rb')
-        csvmodel.readline()
-        text = np.loadtxt(csvmodel, dtype=np.str, delimiter=",")
-        csvmodel.close()
-        for i in range(len(text)):
-            writer.writerow([ int(text[i][0]), answer[ int(text[i][1])-1 ][ dic[text[i][2]] ] ])
-
-        csvfile.close()
